cf15e0f55dea807ba34047a71c61ae661b141524
Fwoofie <142638422+Fwoofie@users.noreply.github.com>
2023-10-20 22:25:18 -0400

    Example

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,9 @@
 
 int main()
 {
-      RandomFwoof::setRandomNumberSeed(time(NULL), 0);
+      RandomFwoof::setRandomNumberSeed(time(NULL), 0); // second argument is for ARS! Leave 0 for auto
       std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-      std::cout << RandomFwoof::getFlag("rerollrandomseed") << std::endl;
+      std::cout << RandomFwoof::getFlag("rerollrandomseed") << std::endl; //automatically true: use setFlag to change! This makes it so when the randomnumbers finish the set sequence, it wil automatically make a new one!
       std::cout << RandomFwoof::generateRandomNumber() << std::endl;
       return 0;
 }
