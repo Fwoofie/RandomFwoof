3081859689fda57b0fb000e9d0606b5dfe45c689
Fwoofie <jamesthehusky1@gmail.com>
2024-03-22 07:59:26 -0500

    Fixed some bias issues and the number range bias

--- src/src/RandomFwoof.cpp
+++ src/src/RandomFwoof.cpp
@@ -1,3 +1,5 @@
+#include <cstddef>
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -11,16 +13,38 @@ std::unordered_map<std::string, bool> flags = {
 int currentRandomIndex = 1;
 int savedApplicationRandomSeed = 0;
 
+std::string fixRandomSeed(std::string givenRandomSeed)
+{
+    std::string fixedRandomSeed = "";
+
+    for (int i = 0; i < givenRandomSeed.length(); i++)
+    {
+        char currentChar = givenRandomSeed[i];
+        if (currentChar != '-')
+        {
+            fixedRandomSeed += currentChar; 
+        }
+    }
+
+    return fixedRandomSeed;
+}
+
 namespace RandomFwoof {
     
     int powerOf(int originalBase, int exponent) 
     {
         int base = originalBase;
-
+        
+        if (exponent == 0)
+        {
+            return 1;
+        }
+        
         for (int i = 1; i < exponent; i++) 
         {
             base = base * originalBase;
         }
+        
         return base;
     }
 
@@ -52,20 +76,26 @@ namespace RandomFwoof {
     {
         return currentRandomIndex;
     }
-
+    
     void setRandomNumberSeed(int tick, int ARS) 
     {
         //std::vector<int>* randomNumberArray = {0};
         std::string stringTick = std::to_string(tick); //intToArray(tick, *randomNumberArray);
         std::string finishedNumber;
-
+        
+        // printf("\nstringTick: %s\n", stringTick.c_str());
+        
         if (ARS == 0) 
         {
-            ARS = time(NULL) * 0.0000004;
+            ARS = time(NULL) * 0.5;
         }
-        
-        savedApplicationRandomSeed = ARS;
 
+        //printf("\nARS: %d\n", ARS);
+
+        savedApplicationRandomSeed = ARS;
+            
+        int tempOperationHolder = time(0) * 0.25;
+        
         for (int i = 0; i < stringTick.length(); i++) {
             int previousIndex;
             int secondPreviousIndex;
@@ -73,7 +103,7 @@ namespace RandomFwoof {
             if (i == 0) 
             {
                 previousIndex = stringTick.length() - 1;
-                secondPreviousIndex = previousIndex - 2;
+                secondPreviousIndex = previousIndex - 1;
             }
             else 
             {
@@ -90,15 +120,18 @@ namespace RandomFwoof {
             }
 
             int currentChar = stringTick[i];
-            currentChar = powerOf(currentChar, stringTick[previousIndex]);
-            currentChar = (unsigned int) currentChar * secondPreviousIndex * previousIndex * ARS;
-
-            finishedNumber.append(std::to_string(currentChar * tick));
+            currentChar = powerOf(currentChar + 1, stringTick[previousIndex] + 1);
+            //printf("\ndebug 1: %d\n", currentChar);
+            currentChar = (unsigned int) currentChar * secondPreviousIndex + 1 * ( stringTick[secondPreviousIndex] * (previousIndex * 0.5)) + 1 * ARS;
+            currentChar += tempOperationHolder;
+            //printf("\ndebug 2: %d\n", currentChar); 
+            std::string localFinishedString = std::to_string(currentChar * tick);
+            finishedNumber.append(localFinishedString.substr(1, localFinishedString.length() - 1));
 
         }
 
-        randomNumberSeed = finishedNumber;
-
+        randomNumberSeed = fixRandomSeed(finishedNumber);
+        //printf("\nrandomNumberSeed: %s\n", randomNumberSeed.c_str());
         //std::cout << "The finished product! > " <<  finishedNumber << std::endl;
     }
 
@@ -121,6 +154,21 @@ namespace RandomFwoof {
 
     int generateRandomNumberRange(int min, int max)
     {
+        
+        int range = max - min;
+        
+        int currentRoll = 0;
+        for (int i = 0; i < range; i++)
+        {
+            if (generateRandomNumber() > 4)
+            {
+                currentRoll++;
+            }
+        }
+        
+        return currentRoll + min;
+
+        /*
         int range = max - min;
 
         int randomNumber = 0;
@@ -139,6 +187,7 @@ namespace RandomFwoof {
         randomNumber = (currentRolls * 0.1);
 
         return randomNumber + min;
+        */
     }
 
 }
